52415037fb412c79724d76ad40d3336c09271578
StounhandJ <rmarinicev@gmail.com>
2022-02-21 19:14:46 +0300

    fixes | print error

--- main.go
+++ main.go
@@ -44,7 +44,10 @@ func getSummoners(matchSummoners *[]model.Summoner, summoners *[]model.Summoner)
 			_ = db.Db.Model(&s).Where("puuid = ?", summoner.Puuid).Select()
 			if s.Id == "" {
 				s = riot.GetSummonerPuuid(summoner.Puuid)
-				_, _ = db.Db.Model(&s).Insert()
+				_, err := db.Db.Model(&s).Insert()
+				if err != nil {
+					fmt.Println("Error (saveMatch): ", err)
+				}
 			}
 		} else {
 			s = (*summoners)[0]
@@ -65,8 +68,15 @@ func findSummoner(summoners *[]model.Summoner, summoner *model.Summoner) int {
 }
 
 func saveMatch(match *model.Match) {
-	_, _ = db.Db.Model(&match).Insert()
+	_, err := db.Db.Model(match).Insert()
+	if err != nil {
+		fmt.Println("Error (saveMatch): ", err)
+		return
+	}
 	for _, summoner := range match.Summoners {
-		_, _ = db.Db.Model(&model.MatchSummoner{MatchID: match.IdDB, SummonerID: summoner.Id}).Insert()
+		_, err = db.Db.Model(&model.MatchSummoner{MatchID: match.IdDB, SummonerID: summoner.Id}).Insert()
+		if err != nil {
+			fmt.Println("Error (saveMatch): ", err)
+		}
 	}
 }
